a925ee0bd054e647ba389ae9d7d74919706fd7af
tokiloki <codebadger1@gmail.com>
2022-01-12 10:28:46 +0200

    basic javascript implement

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('hello world!');
+const bookTitle = document.querySelector('#title');
+const bookAuthor = document.querySelector('#author');
+const bookStatus = document.querySelector('#status');
+const tableContainer = document.querySelector('.book-table-container');
+const tableBody = document.querySelector('#book-table-body');
+
+let library = [];
+
+function Book(title, author, readStatus) {
+
+    this.title = title;
+    this.author = author;
+    this.readStatus = readStatus;
+
+}
