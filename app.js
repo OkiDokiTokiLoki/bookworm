65d374fd888f6aae3fa663836b3b7c2f1f197c3c
tokiloki <codebadger1@gmail.com>
2022-01-12 11:36:45 +0200

    code cleanup and refactoring

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
 const bookTitle = document.querySelector('#title');
 const bookAuthor = document.querySelector('#author');
 const bookStatus = document.querySelector('#status');
+const submitBook = document.querySelector('.submit');
 const tableContainer = document.querySelector('.book-table-container');
 const tableBody = document.querySelector('#book-table-body');
 
 let library = [];
+let newBook;
+
+submitBook.addEventListener('click', addBookToLibrary());
 
 function Book(title, author, readStatus) {
-
     this.title = title;
     this.author = author;
     this.readStatus = readStatus;
+}
 
+function addBookToLibrary(){
+    e.preventDefault();
+    newBook = new Book(title, author, readStatus);
+    library.push(newBook);
+    render();
+    form.reset();
+}
+
+function render() {
+    const books = document.querySelectorAll('.book');
+    books.forEach(book => tableBody.removeChild(book));
+   
+    for (let i=0; i<library.length; i++){
+        createBook(library[i]);
+    }
 }
