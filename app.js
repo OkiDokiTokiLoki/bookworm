406117fe54ab7388501a2b7a9410dc6385d08440
tokiloki <codebadger1@gmail.com>
2022-01-14 15:55:57 +0200

    improved Book object and readBtn function

--- app.js
+++ app.js
@@ -6,29 +6,41 @@ const tableContainer = document.querySelector('.book-table-container');
 const tableBody = document.querySelector('#book-table-body');
 
 let library = [];
-let newBook;
 
 submitBook.addEventListener('click', addBookToLibrary());
 
-function Book(title, author, readStatus) {
-    this.title = title;
-    this.author = author;
-    this.readStatus = readStatus;
+function Book(title, author, readTheBook){
+    this.title = title
+    this.author = author
+    this.readTheBook = readTheBook
+    this.info = () => {
+        return `${title}, ${author}, ${readTheBook}`
+    }
 }
 
+// const book1 = new Book('Harry Potter', 'JK Rowling', 'yes');
+// const book2 = new Book('GOT', 'GRRM', 'no');
+// console.log(`book 1: ${book1.info()}  || book 2: ${book2.info()}`)
+
 function addBookToLibrary(){
-    e.preventDefault();
-    newBook = new Book(title, author, readStatus);
+    //e.preventDefault();
+    let newBook = new Book(title, author, readTheBook);
     library.push(newBook);
-    render();
+    console.log(library);
+    // render();
     form.reset();
 }
 
-function render() {
-    const books = document.querySelectorAll('.book');
-    books.forEach(book => tableBody.removeChild(book));
+// function render() {
+//     const books = document.querySelectorAll('.book');
+//     books.forEach(book => tableBody.removeChild(book));
    
-    for (let i=0; i<library.length; i++){
-        createBook(library[i]);
-    }
-}
+//     for (let i=0; i<library.length; i++){
+//         createBook(library[i]);
+//     }
+// }
+
+// readBtn.addEventListener('click', () => { 
+//     item.read = !item.read; 
+//     render();
+// }); 
